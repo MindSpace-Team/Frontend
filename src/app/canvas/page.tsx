7bc4c6f5d2e7b9aa58ddfce858e6235698cf0686
심민 <tlaals44@naver.com>
2025-06-15 21:08:30 +0900

    css 변경

--- src/app/canvas/page.tsx
+++ src/app/canvas/page.tsx
@@ -3,7 +3,7 @@ import NodeManager from "@/components/controls/NodeManager";
 
 export default function CanvasPage() {
   return (
-    <main style={{ width: "100vw", height: "100vh", background: "#111926" }}>
+    <main style={{ width: "100vw", height: "100vh", background: "#000000" }}>
       <NodeManager />
     </main>
   );
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,16 +1,14 @@
 import Login from "@/components/home/Login";
 import { StarsBackground } from "@/components/ui/stars-background";
 import { ShootingStars } from "@/components/ui/shooting-stars";
-import Header from "@/components/header/Header";
 import "@/styles/globals.css";
 
 export default function LoginPage() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen bg-slate-900 text-white">
-      <Login />
+    <>
       <StarsBackground />
       <ShootingStars />
-      <Header />
-    </main>
+      <Login />
+    </>
   );
 }
